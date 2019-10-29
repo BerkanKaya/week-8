fcc4cbd1f39642cceaf2b810285ae0e0a5c062b2
BerkanKaya <99059255@mydavinci.nl>
2019-10-29 13:41:39 +0100

    opdracht 2 lab 1

---
+++ js2.js
@@ -0,0 +1,35 @@
+function addition(number1, number2){
+	total = number1 + number2;
+	return (total);
+}
+document.write("10 + 12 = " + addition (10, 12));
+
+function subtraction(number3, number4){
+	total = number3 - number4;
+	return (total);
+}
+document.write("<br> 58 - 34 = " + subtraction (58, 34));
+
+function multiplication(multi, multi1){
+	total = multi * multi1;
+	return (total);
+}
+document.write("<br> 6 x 7 = " + multiplication (6, 7));
+
+function division(multi2, multi3){
+	total = multi2 / multi3;
+	return (total);
+}
+document.write("<br> 144 : 12 = " + division (144, 12));
+
+function increment(divi, divi1){
+	total = divi + divi1;
+	return (total);
+}
+document.write("<br> 12 + 1 = " + increment (12, 1));
+
+function decrement(divi, divi1){
+	total = divi + divi1;
+	return (total);
+}
+document.write("<br> 34 - 1 = " + decrement (34, 1));
