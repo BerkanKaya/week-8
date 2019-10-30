0b00e0a26b2430b122a3a7fce79e9c0ae2f2295c
BerkanKaya <99059255@mydavinci.nl>
2019-10-30 11:13:09 +0100

    opdracht 4 lab af

--- js4.js
+++ js4.js
@@ -2,13 +2,22 @@ function tafels(){
 	tafelvraag = prompt("voer 1, 2 of 3 in")
 	if (tafelvraag == "1") {
 		for(var i=0; i<=10; i++)
-			document.write("1 x"+i+"=" + 1*i+"<br>");
+			document.write("<br> 1 x"+i+"=" + 1*i+"<br>");
 }
 else if (tafelvraag == "2") {
+
+	for(var i=0; i<=10; i++)
+			document.write(" 1 x"+i+"=" + 1*i+"<br>");
 	for(var j=0; j<=10; j++)
-		document.write("2 x"+j+"=" + 2*j+"<br>")
+		
+		document.write(" 2 x"+j+"=" + 2*j+"<br>")
+
 }
 else if (tafelvraag == "3") {
+	for(var i=0; i<=10; i++)
+			document.write(" 1 x"+i+"=" + 1*i+"<br>");
+	for(var j=0; j<=10; j++)
+		document.write("2 x"+j+"=" + 2*j+"<br>")
 	for(var d=0; d<=10; d++)
 		document.write("3 x"+d+"=" + 3*d+"<br>")
 }
