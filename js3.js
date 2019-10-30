b2ce72b347f46fa227dcf43adbba5bcd5a6e0506
BerkanKaya <99059255@mydavinci.nl>
2019-10-30 09:45:10 +0100

    opdracht 3 lab 1 af

--- js3.js
+++ js3.js
@@ -1,10 +1,21 @@
-function naam(name, age){
-	return (name);
-	return (age);
+function naam(){
+	var name = prompt('Wat is uw naam?');
+	var age = prompt('Wat is uw leeftijd?');
+
+if(name !== "stop"){
+	document.write("<br> Naam = " + name);
+	naam();
+
+	if(age !== "stop"){
+	document.write("<br> leeftijd = " + age);
 }
-var name = prompt('Wat is uw naam?');
-var age = prompt('Wat is uw leeftijd?');
-document.write("Naam = " + naam (name));
-document.write("<br> Leeftijd = " + naam (age));
+
+	
+}
+}
+
+	
+
+naam();
 
 
